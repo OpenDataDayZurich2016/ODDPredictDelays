bf864b69d39dc3699e5e54dc921ae4209a4f84df
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-04 16:55:10 +0100

    Revert "better model"
    This reverts commit 8dd3bf1d43bed78accfae03bd772b81920878354.

--- src/model.R
+++ src/model.R
@@ -1,13 +1,14 @@
 create_model <- function(filtered_delay_data) {
-  res <- lm(
-    delay ~ weekday + vehicle_type + factor(temp > 10) + factor(precipitation > 0),
-    filtered_delay_data, model = FALSE, qr = TRUE, y = FALSE
+  delay <- filtered_delay_data
+  fit <- lm(
+    delay ~ weekday + vehicle_type + temp + precipitation,
+    delay, model = FALSE, qr = TRUE, y = FALSE
   )
-  print(summary(res))
-  res$rested.values <- NULL
-  res$residuals <- NULL
-  res$na.action <- NULL
-  res$effects <- NULL
-  res$qr$qr <- NULL
-  res
+  print(summary(fit))
+  fit$fitted.values <- NULL
+  fit$residuals <- NULL
+  fit$na.action <- NULL
+  fit$effects <- NULL
+  fit$qr$qr <- NULL
+  fit
 }

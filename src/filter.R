557f9e971b657c9696ea4a91eb4906138aed7c83
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-04 14:20:13 +0100

    organize

--- src/filter.R
+++ src/filter.R
@@ -1,7 +1,19 @@
 add_delay_data_attributes <- function(delay_data) {
   delay_data %>%
+    add_weekday %>%
+    add_delay
+}
+
+add_weekday <- function(delay_data) {
+  delay_data %>%
     mutate(
-      weekday = factor(lubridate::wday(betriebsdatum %>% as.Date(format = "%d.%m.%Y"))),
-      delay = ist_an_von - soll_an_von
+      weekday = factor(lubridate::wday(betriebsdatum %>% as.Date(format = "%d.%m.%Y")))
     )
+}
+
+add_delay <- function(delay_data) {
+  mutate(
+    delay = ist_an_von - soll_an_von,
+    delay_next = ist_an_nach1 - soll_an_nach
+  )
 }

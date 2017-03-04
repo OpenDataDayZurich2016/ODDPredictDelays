cab0a09844b13fedd99321355d0870848ae38f16
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-04 12:09:22 +0100

    rename

--- src/filter.R
+++ src/filter.R
@@ -1,4 +1,4 @@
-filter_delay_data <- function(delay_data) {
+add_delay_data_attributes <- function(delay_data) {
   delay_data %>%
     mutate(
       weekday = factor(lubridate::wday(betriebsdatum %>% as.Date(format = "%d.%m.%Y"))),

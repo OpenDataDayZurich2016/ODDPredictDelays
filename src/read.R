9cf2dff7f8cf2318d9166fe0e343f84caf2de564
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-04 12:58:36 +0100

    filter delay data columns

--- src/read.R
+++ src/read.R
@@ -13,7 +13,20 @@ read_soll_ist <- function(timeslots, year, month) {
 }
 
 read_soll_ist_one <- function(path) {
-  data <- readr::read_delim(path, delim = ",")
+  data <- readr::read_delim(
+    path,
+    delim = ",",
+    col_types = cols(
+      fahrzeug = col_skip(), kurs = col_skip(), halt_diva_von = col_skip(),
+      datum_von = col_skip(), halt_kurz_von1 = col_skip(), seq_nach = col_skip(),
+      halt_diva_nach = col_skip(), halt_punkt_diva_nach = col_skip(),
+      halt_kurz_nach1 = col_skip(), datum_nach = col_skip(), fahrt_id = col_skip(),
+      fahrweg_id = col_skip(), fw_no = col_skip(), fw_typ = col_skip(),
+      fw_kurz = col_skip(), fw_lang = col_skip(), umlauf_von = col_skip(),
+      halt_id_von = col_skip(), halt_id_nach = col_skip(),
+      halt_punkt_id_nach = col_skip()
+    )
+  )
   names(data)[[1]] <- "linie"
   data
 }

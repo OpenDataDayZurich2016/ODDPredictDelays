e228088ecad73e4f26b9e011d77b5a706741ee72
Kirill Müller <krlmlr+r@mailbox.org>
2017-03-04 12:00:10 +0100

    read five weeks of November 2016

--- src/read.R
+++ src/read.R
@@ -1,4 +1,18 @@
-read_soll_ist <- function(path) {
+read_soll_ist <- function(timeslots, year, month) {
+  year <- 2016
+  month <- 11
+  first <- as.Date(sprintf("%s-%s-01", year, month))
+  last <- as.Date(sprintf("%s-%s-30", year, month))
+  our_timeslots <-
+    timeslots %>%
+    filter(as.Date(from) < last & as.Date(to) > first)
+
+  paths <- file.path("raw/data/delay_data", our_timeslots$filename)
+  all_datasets <- parallel::mclapply(paths, read_soll_ist_one)
+  bind_rows(all_datasets)
+}
+
+read_soll_ist_one <- function(path) {
   data <- readr::read_delim(path, delim = ",")
   names(data)[[1]] <- "linie"
   data

ccbba4f18175055ce477a8bc27bd0f63353529ff
HeidiSeibold <heidi.seibold@uzh.ch>
2017-06-01 16:57:32 +0200

    making upload work by deleting collection.date

--- upload_openml/openml_delaydat.R
+++ upload_openml/openml_delaydat.R
@@ -19,23 +19,51 @@ openml_delaydat <- mutate(openml_delaydat,
                           hour = hour(time),
                           dayminute = hour * 60 + minute(time))
 
+# ### Upload subsample
+# set.seed(2017)
+# ss <- sample(1:nrow(openml_delaydat), size = 30)
+# openml_delaydat_s <- as.data.frame(openml_delaydat[ss, ])
+# openml_delaydat_s$time <- as.character(openml_delaydat_s$time)
+# 
+# dsc_s <- "Subsample of Zurich public transport delay data 2016-10-30 03:30:00 CET - 2016-11-27 01:20:00 CET cleaned and prepared at Open Data Day 2017."
+# 
+# delay_descr_s <- makeOMLDataSetDescription(
+#   name = "subsample_delays_zurich_transport",
+#   description = dsc_s,
+#   contributor = "https://github.com/OpenDataDayZurich2016/ODDPredictDelays/graphs/contributors",
+#   # collection.date = "2016-10-30 03:30:00 CET - 2016-11-27 01:20:00 CET",
+#   licence = "CCZero",
+#   default.target.attribute = "delay",
+#   citation = "https://github.com/OpenDataDayZurich2016/ODDPredictDelays",
+#   original.data.url = "https://data.stadt-zuerich.ch/dataset/vbz-fahrzeiten-ogd"
+# )
+# 
+# oml_delay <- makeOMLDataSet(desc = delay_descr_s,
+#                             data = openml_delaydat_s,
+#                             target.features = "delay")
+# 
+# uploadOMLDataSet(oml_delay, confirm.upload = FALSE)
 
 
+### Upload all
 dsc <- "Zurich public transport delay data 2016-10-30 03:30:00 CET - 2016-11-27 01:20:00 CET cleaned and prepared at Open Data Day 2017."
 
 delay_descr <- makeOMLDataSetDescription(
   name = "delays_zurich_transport",
   description = dsc,
   contributor = "https://github.com/OpenDataDayZurich2016/ODDPredictDelays/graphs/contributors",
-  collection.date = "2016-10-30 03:30:00 CET - 2016-11-27 01:20:00 CET",
+  # collection.date = "2016-10-30 03:30:00 CET - 2016-11-27 01:20:00 CET",
   licence = "CCZero",
   default.target.attribute = "delay",
   citation = "https://github.com/OpenDataDayZurich2016/ODDPredictDelays",
   original.data.url = "https://data.stadt-zuerich.ch/dataset/vbz-fahrzeiten-ogd"
   )
 
+openml_delaydat <- as.data.frame(openml_delaydat)
+openml_delaydat$time <- as.character(openml_delaydat$time)
+
 oml_delay <- makeOMLDataSet(desc = delay_descr,
-                            data = as.data.frame(openml_delaydat),
+                            data = openml_delaydat,
                             target.features = "delay")
 
 uploadOMLDataSet(oml_delay)
